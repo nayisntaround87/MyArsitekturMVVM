237e8dd0ae4148e7361b606c1dae2d29b665ce71
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 17:13:01 +0700

    membuat fun siswa app

--- app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -2,6 +2,9 @@ package com.example.myarsitekturmvvm.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myarsitekturmvvm.viewmodel.SiswaViewModel
 
 enum class Navigasi {
     Formulir,
@@ -11,5 +14,8 @@ enum class Navigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaApp(
-
-)
+    //edit 1 : parameter viewModel
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
