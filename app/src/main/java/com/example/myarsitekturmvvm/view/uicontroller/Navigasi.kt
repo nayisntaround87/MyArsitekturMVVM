dc5f733c4dcd38b23f868aa8c6f210dcaf56fc51
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 17:18:57 +0700

    mnambahkan private fun

--- app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -57,4 +57,9 @@ fun SiswaApp(
     }
 }
 
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
 
