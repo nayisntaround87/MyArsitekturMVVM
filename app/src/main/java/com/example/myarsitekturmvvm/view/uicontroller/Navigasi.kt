818035e0cc7bc67ed814754e699b384fe53f0284
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 15:16:08 +0700

    membuat siswa app

---
+++ app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -0,0 +1,15 @@
+package com.example.myarsitekturmvvm.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+
+)
