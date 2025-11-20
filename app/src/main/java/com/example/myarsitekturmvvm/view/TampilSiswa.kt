85fba2f3a36edde50975360271249bbddc68d042
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 14:26:08 +0700

    membuat column isi ruang

--- app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
@@ -1,8 +1,21 @@
 package com.example.myarsitekturmvvm.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import com.example.myarsitekturmvvm.model.Siswa
+import kotlinx.serialization.builtins.PairSerializer
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -11,5 +24,25 @@ fun TampilSiswa(
     statusUiSiswa: Siswa,
     onBackButtonClicked:()-> Unit
 ){
+    //edit 2 : Items pair
+    val items = listOf(
+        Pair(stringResource("Nama Lengkap"), statusUiSiswa.nama),
+        Pair(stringResource(id = "Jenis Kelamin"), statusUiSiswa.gender),
+        Pair(stringResource("Alamat"), statusUiSiswa.alamat)
+    )
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(id = "Data Siswa"), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.purple_500)),
 
+            )
+        }) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+
+        }
+    }
 }
