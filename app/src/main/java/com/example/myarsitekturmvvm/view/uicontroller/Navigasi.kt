77cf4006de48f003529f6f72f5aa864f899076cd
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 17:15:18 +0700

    Tambahkan variable konteks

--- app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -1,9 +1,15 @@
 package com.example.myarsitekturmvvm.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myarsitekturmvvm.model.DataJK
+import com.example.myarsitekturmvvm.view.FormSiswa
 import com.example.myarsitekturmvvm.viewmodel.SiswaViewModel
 
 enum class Navigasi {
@@ -19,3 +25,17 @@ fun SiswaApp(
     viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    Scaffold { isiRuang ->
+        //edit 2 : Tambahkan variabel uiState
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding( isiRuang)) {
+            composable(route = Navigasi.Formulir.name) {
+                //edit 3 : Tambahkan variable konteks
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = DataJK.Jenis.map { id ->
+                        konteks.getString(id)
+                    },
